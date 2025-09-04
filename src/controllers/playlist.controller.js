486c2d9f9824c7ff0c8ee50e9caf647bcaec5cb4
Thiago <martinsmthiago@gmail.com>
2025-09-04 20:49:01 -0300

    Add video to playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -5,7 +5,7 @@ const ApiResponse = require("../utils/ApiResponse");
 const asyncHandler = require("../utils/asyncHandler");
 
 const createPlaylist = asyncHandler(async (req, res) => {
-  const { name, description, isPublic = true } = req.body;
+  const { name, description, isPublic = true } = req.body; 
   if (!name || name.trim() === "") {
     throw new ApiError(400, "Playlist name is required");
   }
@@ -20,7 +20,33 @@ const createPlaylist = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(201, playlist, "Playlist created"));
 });
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+  const { playlistId, videoId } = req.params;
+  if (!playlistId || !videoId) {
+    throw new ApiError(400, "Playlist and video Id are required");
+  }
+  // Check if playlist exists and belongs to user
+  const playlist = await Playlist.findOne({
+    _id: playlistId,
+    owner: req.user._id,
+  });
+  if (!playlist) {
+    throw new ApiError(404, "Playlist not found or you don't have permission");
+  }
+
+  // Check if video is already in playlist
+  if (playlist.videos.includes(videoId)) {
+    throw new ApiError(400, "Video already exists in the playlist");
+  }
+  //Add video to playlist
+  playlist.videos.push(videoId);
+  await playlist.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Video added to playlist"));
+});
 
 module.exports = {
   createPlaylist,
+  addVideoToPlaylist
 };
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { createPlaylist } = require('../controllers/playlist.controller');
+const {
+  createPlaylist,
+  addVideoToPlaylist,
+} = require('../controllers/playlist.controller');
 const verifyJWT = require('../middlewares/auth.middleware');
 
 const playlistRouter = express.Router();
 
 //Create playlist
 playlistRouter.post('/', verifyJWT, createPlaylist);
+
+//Add/remove videos from playlist
+playlistRouter.post('/:playlistId/add/:videoId', verifyJWT, addVideoToPlaylist);
 
 module.exports = playlistRouter;
